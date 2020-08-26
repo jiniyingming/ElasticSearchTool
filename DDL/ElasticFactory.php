da72b510fdd3ec2aa25623bb50f6931033ef4a7a
jinyingming <jinyingming@gongchangtemai.com.cn>
2020-08-26 13:34:40 +0800

    搜索工具模型初始

---
+++ DDL/ElasticFactory.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace App\Libs\ElasticSearchTool\DDL;
+
+
+use App\Libs\ElasticSearchTool\ElasticTool;
+use Elasticsearch\Client;
+
+class ElasticFactory
+{
+	private static $client;
+
+	public function __construct(Client $elasticTool)
+	{
+		self::$client = $elasticTool;
+	}
+
+	/**
+	 * @param $map
+	 * @return array
+	 * 创建索引
+	 */
+	public function create($map): array
+	{
+		return self::$client->create($map);
+	}
+
+	/**
+	 * @param $params
+	 * @return array
+	 */
+	public function delete($params): array
+	{
+		return self::$client->delete($params);
+	}
+
+}
